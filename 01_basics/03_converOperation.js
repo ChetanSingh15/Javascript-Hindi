960de8d421522c3e2914a40bf5d19e43ba83f734
ChetanSingh15 <145979815+ChetanSingh15@users.noreply.github.com>
2024-02-16 08:17:16 +0000

    ConversionOperations2

---
+++ 01_basics/03_converOperation.js
@@ -0,0 +1,54 @@
+let score = "true"
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber = Number(score)
+
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1 ; false => 0
+// null => 0
+// undefined => undefined
+
+let isLoggedIn = ""
+
+let BooleanLoggedIn = Boolean(isLoggedIn)
+// console.log(BooleanLoggedIn)
+
+// 1 => true; 0 => false
+// "" => false
+// "Hi" => true
+
+let n=33
+
+let StringNumber = String(n)
+// console.log(StringNumber)
+// console.log(typeof StringNumber)
+
+
+// ********************************************** Operations *********************************************
+
+let value = 3
+let negValue= -value
+// console.log(negValue)
+
+let str1 = "Hi"
+let str2 = " Hello"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1"+2)
+// console.log(1+"2");
+// console.log("1"+2+2);
+// console.log(1+2+"2");
+
+let gamecounter=100;
+++gamecounter;
+console.log(gamecounter);
+
+
